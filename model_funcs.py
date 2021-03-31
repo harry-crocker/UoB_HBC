9ba7fadfa1631bf2f4192c5e9569b571e98a517f
Harry Crocker <Harry-Crocker@live.co.uk>
2021-03-31 17:43:24 +0100

    New run, update model config

--- model_funcs.py
+++ model_funcs.py
@@ -34,16 +34,16 @@ if dev_mode:
 config = Config_file()
 config.num_modules = 6 # 6
 config.epochs = 50 # PTB-XL = 50
-config.lr = 1e-2  # 1e-2
+config.lr = 3e-3  # 1e-2
 config.batch_size = 128  # PTB-XL = 128
 config.optimizer='AdamWeightDecay'
 config.wd = 1e-2 # Float
-config.Window_length = 250 # 250
+config.Window_length = 125 # 250
 config.lap = 0.5
 config.loss_func = 'BC'   # BC Or F1
 config.SpE = 1 # 1
 config.filters = 64
-config.kernel_sizes = [3, 7, 17] #[9, 23, 49]
+config.kernel_sizes = [3, 5, 11] #[9, 23, 49]
 config.head_nodes = 2048
 config.val_split = 0.1
 
