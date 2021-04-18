8ae6369337d2eacd0ac53bcd4c80ae1994f5b00c
Harry Crocker <Harry-Crocker@live.co.uk>
2021-04-18 16:28:36 +0100

    change for wandb

--- wide_features.py
+++ wide_features.py
@@ -11,33 +11,33 @@ if __name__ == '__main__':
 
     data_directory = sys.argv[1]
 
-	header_files, _ = find_challenge_files(data_directory)
+    header_files, _ = find_challenge_files(data_directory)
 
-	age_count = 0
-	sex_count = 0
-	total_count = 0
-	age_nan = 0
-	sex_nan = 0
+    age_count = 0
+    sex_count = 0
+    total_count = 0
+    age_nan = 0
+    sex_nan = 0
 
-	for file in header_files:
-		header = load_header(file)
+    for file in header_files:
+        header = load_header(file)
 
-		total_count += 1
+        total_count += 1
 
-		age = get_age(header)
-		sex = get_sex(header)
+        age = get_age(header)
+        sex = get_sex(header)
 
-		if age:
-			age_count += 1
-		else:
-			age_nan += 1
+        if age:
+            age_count += 1
+        else:
+            age_nan += 1
 
 
-		if sex:
-			sex_count += 1
-		else:
-			sex_nan +=1 
+        if sex:
+            sex_count += 1
+        else:
+            sex_nan +=1 
 
-	print('Ages', age_count, age_nan)
-	print('Sexes', sex_count, sex_nan)
-	print('Total', total_count)
+    print('Ages', age_count, age_nan)
+    print('Sexes', sex_count, sex_nan)
+    print('Total', total_count)
