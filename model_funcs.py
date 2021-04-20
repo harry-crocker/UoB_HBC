756e7c671b3e85381b75435357a4d88ddba166c4
Harry Crocker <Harry-Crocker@live.co.uk>
2021-04-20 14:26:10 +0100

    final runs

--- model_funcs.py
+++ model_funcs.py
@@ -48,7 +48,7 @@ config.filters = 32
 config.kernel_sizes = [3, 7, 17] #[9, 23, 49]
 config.head_nodes = 2048
 config.val_split = 0.1
-config.epochs = 50
+config.epochs = 1
 
 
 def load_data(header_files, recording_files, leads, classes):
